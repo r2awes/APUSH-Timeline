6dfc2a230e6a4ae2047e9ca6b56162ffbef2e375
r2awes <r2awes@gmail.com>
2019-06-22 23:10:06 -0400

    Updates

---
+++ precache-manifest.3a96a6afd7ce55810f99a90697e98f61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0556b1fad99e696ea5e6928a50f5aea1",
+    "url": "/APUSH-Timeline/index.html"
+  },
+  {
+    "revision": "6c66df6f150ca8dfe23d",
+    "url": "/APUSH-Timeline/static/css/main.48052f9c.chunk.css"
+  },
+  {
+    "revision": "0f60c177e006aff6cb81",
+    "url": "/APUSH-Timeline/static/js/2.e83785af.chunk.js"
+  },
+  {
+    "revision": "6c66df6f150ca8dfe23d",
+    "url": "/APUSH-Timeline/static/js/main.e908bc70.chunk.js"
+  },
+  {
+    "revision": "657e2a40672972467ea9",
+    "url": "/APUSH-Timeline/static/js/runtime~main.73ccbcaa.js"
+  }
+]);
